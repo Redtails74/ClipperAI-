8d99100ae4ee0d5d5a03ab5fd4718e8895f60412
Redtails74 <pep4474@me.com>
2024-10-12 11:52:40 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 
 # Initialize Flask app
 app = Flask(__name__)
-CORS(app, resources={r"/api/*": {"origins": "*"}})
+CORS(app, resources={r"/api/*": {"origins": "https://https://Redtails74.github.io/ClipperAI-"}})
 
 # Set up Hugging Face API key and model
 API_KEY = os.getenv('HUGGINGFACE_API_KEY', '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')
