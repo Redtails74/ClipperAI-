836c8c3e5bb99e2d2034aaad5b39321df7389e52
Redtails74 <pep4474@me.com>
2024-10-15 20:05:13 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ from flask_cors import CORS
 import os
 from transformers import pipeline
 
-aapp = Flask(__name__)
+app = Flask(__name__)
+
 CORS(app, resources={r"/api/*": {"origins": ["https://clipper.ai"]}})
 
 app.config['PREFERRED_URL_SCHEME'] = 'https'
@@ -39,3 +40,4 @@ def chat():
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8000)
+
