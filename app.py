d26020a4fc6c8e4413da7a7a25d6c3003806fa77
Redtails74 <pep4474@me.com>
2024-10-26 10:36:49 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,10 @@ import os
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-app = Flask(__name__)
+app = Flask(__name__, 
+            static_url_path='/static', 
+            static_folder='./static', 
+            template_folder='./templates')
 CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 # Load environment variables
